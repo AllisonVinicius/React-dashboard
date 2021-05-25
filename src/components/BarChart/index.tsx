c28c74bdf7efbee89890ea06c06042384c807571
AllisonVinicius <allisonvinicius63@gmail.com>
2021-05-25 13:10:47 -0400

    funcao para grafico em barras

--- src/components/BarChart/index.tsx
+++ src/components/BarChart/index.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
-import { LadoDireito, LadoEsquerdo } from '../Pichart/stylePieChart'
+import { LadoDireito, LadoEsquerdo,Legend,LegendContainer } from '../Pichart/stylePieChart'
 import { Container } from './styleBarchart'
-import { ResponsiveContainer, BarChart,Bar,Cell, Tooltip} from 'recharts';
-
+import {ResponsiveContainer, BarChart,Bar,Cell, Tooltip} from 'recharts';
+import FormatValor from '../../utils/formatarValores';
 
 interface IBarChartProps {
     
@@ -22,23 +22,35 @@ export const  BarChartBox: React.FC<IBarChartProps> = ({
         <Container>
             <LadoEsquerdo>
                 <h2> {titulo}</h2>
+                <LegendContainer>
+                    { 
+                        data.map((p) => (    
+                            <Legend key={p.name} color={p.color}>
+                                <div>{p.percent}%</div>
+                                <span>{p.name}</span>
+                            </Legend>
+                        ))
+                    }
+                </LegendContainer>
             </LadoEsquerdo>
 
             <LadoDireito>
                 <ResponsiveContainer>
                     <BarChart data={data}>
-                        <Bar datakey="valor">
-                            {data.map((indicator) => (
-                                <Cell
-                                    key={indicator.name}
-                                    cursor="point"
-                                    fill={indicator.color}
-                                />
-                            ))}
+                        <Bar dataKey="valor">
+                            {
+                                data.map((indicator) => (
+                                    <Cell
+                                        key={indicator.name}
+                                        fill={indicator.color}
+                                        
+                                    />
+                                ))
+                            }
                         </Bar>
+                        
 
                     </BarChart>
-
                 </ResponsiveContainer>
             </LadoDireito>
 
