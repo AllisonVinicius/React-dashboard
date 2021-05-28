3fca2de1272841f7d7e9aac4d2130bd5f6689493
AllisonVinicius <allisonvinicius63@gmail.com>
2021-05-27 21:08:08 -0400

    adiconando interface do toogle

--- src/components/Toogle/index.tsx
+++ src/components/Toogle/index.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import { Container, ToogleLabel,ToogleSelector } from './style';
 
 
+interface IToogleProps {
+    labelLeft: string;
+    labelRight: string;
+    checked: boolean;
+    onChance(): void;
+}
 
-
-const Toogle: React.FC = () => (
+const Toogle: React.FC<IToogleProps> = ({
+    labelLeft,
+    labelRight,
+    checked,
+    onChance
+}) => (
     <Container>
         <ToogleLabel>Ligth</ToogleLabel>
         <ToogleSelector
 
-        checked
+        checked = {checked}
         uncheckedIcon= {false}
         checkedIcon={false}
-        onChange={() => console.log('mudou')}
+        onChange={onChance}
         />
         <ToogleLabel>Dark</ToogleLabel>
     </Container>
