7e4c4e32cd45e94542bdbc4bf483e3aca632fd18
AllisonVinicius <allisonvinicius63@gmail.com>
2021-05-28 22:15:30 -0400

    guardando localstoge

--- src/hooks/theme.tsx
+++ src/hooks/theme.tsx
@@ -35,16 +35,18 @@ interface ITheme{
 
 const ThemeContext = createContext<IThemeContext>({} as IThemeContext);
 
-const ThemeProvider: React.FC = ({ children}) => {
+const ThemeProvider: React.FC = ({ children }) => {
     const [theme, setTheme] = useState<ITheme>(dark);
 
     const alterarTheme = () => {
         if(theme.title === 'dark'){
             setTheme(light);
+            localStorage.setItem('@react-dashboard:theme',JSON.stringify(light));
         }else {
             setTheme(dark);
+            localStorage.setItem('@react-dashboard:theme',JSON.stringify(dark));
         }
-    }
+    };
 
     return (
         <ThemeContext.Provider value = {{ alterarTheme , theme}}>
