ac7b2b68907e3e0307a3b080c02584b01a390398
AllisonVinicius <allisonvinicius63@gmail.com>
2021-05-29 15:56:17 -0400

    adicionando style da component  de login

---
+++ src/pages/pag_login/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+
+    height: 100vh;
+    display: flex;
+    flex:1;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: ${props => props.theme.colors.primary};
+`;
+
+
+export const Logo = styled.div``;
+
+export const Form = styled.form``;
+
+export const FormTitle = styled.h1``;
