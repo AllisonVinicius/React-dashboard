ef792984a06a40edaa6063bb96dfc7ebf0c2bd02
AllisonVinicius <allisonvinicius63@gmail.com>
2021-05-31 21:14:37 -0400

    adicionando altura do container, para exibir o grafico que nao tavae exibindo

--- src/components/HistoryBox/styleBox.ts
+++ src/components/HistoryBox/styleBox.ts
@@ -6,10 +6,11 @@ interface ILegendProps{
 
 export const Container = styled.div`
     width: 100%;
+    height: 340px;
     display: flex;
     flex-direction: column;
     background-color: ${props => props.theme.colors.tertiary};
-    background-color: ${props => props.theme.colors.white};
+    background-color: ${props => props.theme.colors.tertiary};
 
     margin: 10px 0;
     padding: 30px 20px;
