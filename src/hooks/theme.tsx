2887c70c9c58c89254a49466c2eed21d8861e9ee
allisonvinicius <allisonvinicius63@gmail.com>
2022-02-20 21:19:29 -0400

    modified

--- src/hooks/theme.tsx
+++ src/hooks/theme.tsx
@@ -1,22 +1,22 @@
+
+  
 import React, { createContext, useContext, useState } from 'react';
 import dark from '../styles/themes/dark';
 import light from '../styles/themes/light';
 
 
-
 interface IThemeContext {
-    alterarTheme(): void;
+    toggleTheme(): void;
     theme: ITheme;
 }
 
-interface ITheme{
+interface ITheme {
     title: string;
 
-    colors:{
+    colors: {
         primary: string;
         secondary: string;
         tertiary: string;
-
 
         white: string;
         black: string;
@@ -25,45 +25,44 @@ interface ITheme{
         success: string;
         info: string;
         warning: string;
-
-
-    },
+    }
 }
-
 
 const ThemeContext = createContext<IThemeContext>({} as IThemeContext);
 
 const ThemeProvider: React.FC = ({ children }) => {
     const [theme, setTheme] = useState<ITheme>(() => {
-        const themeSaved = localStorage.getItem('@react-dashboard:theme');
+        const themeSaved = localStorage.getItem('@minha-carteira:theme');
 
-        if(themeSaved){
+        if(themeSaved) {
             return JSON.parse(themeSaved);
         }else{
             return dark;
         }
     });
 
-    const alterarTheme = () => {
+    const toggleTheme = () => {
         if(theme.title === 'dark'){
             setTheme(light);
-            localStorage.setItem('@react-dashboard:theme',JSON.stringify(light));
-        }else {
+            localStorage.setItem('@minha-carteira:theme', JSON.stringify(light));
+        }else{
             setTheme(dark);
-            localStorage.setItem('@react-dashboard:theme',JSON.stringify(dark));
+            localStorage.setItem('@minha-carteira:theme', JSON.stringify(dark));
         }
     };
 
     return (
-        <ThemeContext.Provider value = {{ alterarTheme , theme}}>
+        <ThemeContext.Provider value={{ toggleTheme, theme }}>
             {children}
         </ThemeContext.Provider>
     )
 }
 
-function useTheme(): IThemeContext{
+function useTheme(): IThemeContext {
     const context = useContext(ThemeContext);
+
     return context;
 }
+
 
 export { ThemeProvider, useTheme };
