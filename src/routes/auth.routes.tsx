a5204b430d8aa1249ca4f44226ce5518c47b1cba
allisonvinicius <allisonvinicius63@gmail.com>
2022-05-24 15:27:29 -0400

    updattesss

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import SignIn from '../pages/SignIn';
-
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import SignIn from "../pages/SignIn";
 
 const AuthRoutes: React.FC = () => (
-    <Switch>
-        <Route path="/" component={SignIn}/>
-    </Switch>
-
+  <Switch>
+    <Route path="/" component={SignIn} />
+  </Switch>
 );
-
 
 export default AuthRoutes;
